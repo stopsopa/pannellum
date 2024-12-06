63fa29e11052bcf7377dd14fbe44b9ab6a7a6eb8
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2024-12-06 18:36:26 +0000

    update

--- process.sh
+++ process.sh
@@ -2,20 +2,29 @@ magick --help > /dev/null || { echo "magick not found. Install imagemagick."; ex
 
 set -euo pipefail
 
-rm -rf thumb
 rm -rf ls.txt
-rm -rf ls2.txt
-mkdir -p thumb
-for file in *.jpg; do
-    echo ">$file<" ">thumb/${file%.jpg}<"
-    # if [ "${CI}" = "" ]; then
-    #     sips -z 170 340 "$file" --out "thumb/${file%.jpg}.jpg"
-    # else
-        magick "$file" -resize 340x170^ -gravity center -extent 340x170 "thumb/${file##*/}"
-    # fi
 
+mkdir -p thumb
+
+for FILE in *.jpg; do
+
+    TARGET="thumb/${FILE}"
+
+    echo ">$FILE<" ">${TARGET}<"
+
+    if [ -f "${TARGET}" ]; then
+
+        echo "skipping ${FILE} -> ${TARGET}"
+    else
+        echo "processing ${FILE} -> ${TARGET}"
+        # if [ "${CI}" = "" ]; then
+        #     sips -z 170 340 "$FILE" --out "thumb/${FILE}"
+        # else
+            magick "$FILE" -resize 340x170^ -gravity center -extent 340x170 "${TARGET}"
+        # fi
+    fi
+
+    echo "$FILE::$(exiftool "$FILE" | grep 'Create Date' | awk -F': ' '{print $2}')" >> ls.txt
+
+    echo ""
 done
-ls *.jpg > ls.txt
-while IFS= read -r file; do
-    echo "$file::$(exiftool "$file" | grep 'Create Date' | awk -F': ' '{print $2}')"
-done < ls.txt | tee ls2.txt
